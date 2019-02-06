2a7b2588ad84d5edf46365c80d187230c6d0644c
Đậu Quốc Toản <beantoan@gmail.com>
2019-02-06 15:42:50 +0100

    Api to register new account

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -2,37 +2,21 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Http\Api\ApiResponse;
+use App\Http\Api\ErrorApiResponse;
 use App\Models\User;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Validator;
-use Illuminate\Foundation\Auth\RegistersUsers;
 
 class RegisterController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Register Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles the registration of new users as well as their
-    | validation and creation. By default this controller uses a trait to
-    | provide this functionality without requiring any additional code.
-    |
-    */
-
-    use RegistersUsers;
-
-    /**
-     * Where to redirect users after registration.
-     *
-     * @var string
-     */
-    protected $redirectTo = '/home';
+    private $ERROR_INVALID_DATA = 100;
+    private $ERROR_SAVE_DATA = 101;
 
     /**
      * Create a new controller instance.
      *
-     * @return void
      */
     public function __construct()
     {
@@ -40,9 +24,44 @@ class RegisterController extends Controller
     }
 
     /**
+     * Handle a registration request for the application.
+     * @return \Illuminate\Http\Response
+     * @internal param \Illuminate\Http\Request $request
+     */
+    public function register()
+    {
+        $data = request(['email', 'name', 'password', 'password_confirmation']);
+
+        Log::debug([RegisterController::class, 'register()', '$data', $data]);
+
+        $validator = $this->validator($data);
+
+        $apiResponse = new ApiResponse();
+
+        if ($validator->fails()) {
+            $apiResponse->setCode($this->ERROR_INVALID_DATA);
+            $apiResponse->setMsg(__('messages.register_invalid_data_error'));
+            $apiResponse->setData($validator->errors());
+
+            return respondApiError($apiResponse);
+        } else {
+            $user = $this->create($data);
+
+            if ($user) {
+                $apiResponse->setMsg(__('messages.register_save_data_success'));
+                return respondApiSuccess($apiResponse);
+            } else {
+                $apiResponse->setCode($this->ERROR_SAVE_DATA);
+                $apiResponse->setMsg(__('messages.register_save_data_error'));
+                return respondApiError($apiResponse);
+            }
+        }
+    }
+
+    /**
      * Get a validator for an incoming registration request.
      *
-     * @param  array  $data
+     * @param  array $data
      * @return \Illuminate\Contracts\Validation\Validator
      */
     protected function validator(array $data)
@@ -57,7 +76,7 @@ class RegisterController extends Controller
     /**
      * Create a new user instance after a valid registration.
      *
-     * @param  array  $data
+     * @param  array $data
      * @return \App\Models\User
      */
     protected function create(array $data)
