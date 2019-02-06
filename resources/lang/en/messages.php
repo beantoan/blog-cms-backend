18b20e65c293aadce141905ed78a5f91f4663686
Đậu Quốc Toản <beantoan@gmail.com>
2019-02-06 15:45:22 +0100

    Added messages

---
+++ resources/lang/en/messages.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+
+
+    'register_invalid_data_error' => 'Please provide valid data.',
+    'register_save_data_error' => 'Error when creating account.',
+    'register_save_data_success' => 'Account is created successfully.',
+
+];
