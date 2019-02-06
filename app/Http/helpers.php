9c72b87680e95e28efd02475504381db7c21543b
Đậu Quốc Toản <beantoan@gmail.com>
2019-02-06 15:45:10 +0100

    Reformat the code

--- app/Http/helpers.php
+++ app/Http/helpers.php
@@ -8,8 +8,9 @@
 
 use App\Http\Api\ApiResponse;
 
-if (! function_exists('createJwtResponse')) {
-    function createJwtResponse($token) {
+if (!function_exists('createJwtResponse')) {
+    function createJwtResponse($token)
+    {
         return [
             'access_token' => $token,
             'token_type' => 'bearer',
@@ -18,26 +19,30 @@ if (! function_exists('createJwtResponse')) {
     }
 }
 
-if (! function_exists('respondWithToken')) {
-    function respondWithToken($token) {
+if (!function_exists('respondWithToken')) {
+    function respondWithToken($token)
+    {
         return response()->json(createJwtResponse($token));
     }
 }
 
-if (! function_exists('respondApiSuccess')) {
-    function respondApiSuccess(ApiResponse $apiResponse) {
+if (!function_exists('respondApiSuccess')) {
+    function respondApiSuccess(ApiResponse $apiResponse)
+    {
         return response()->json($apiResponse->toArray(), 200);
     }
 }
 
-if (! function_exists('respondApiError')) {
-    function respondApiError(ApiResponse $apiResponse) {
+if (!function_exists('respondApiError')) {
+    function respondApiError(ApiResponse $apiResponse)
+    {
         return response()->json($apiResponse->toArray(), 400);
     }
 }
 
-if (! function_exists('respondUnauthorized')) {
-    function respondUnauthorized(ApiResponse $apiResponse) {
+if (!function_exists('respondUnauthorized')) {
+    function respondUnauthorized(ApiResponse $apiResponse)
+    {
         return response()->json($apiResponse->toArray(), 401);
     }
 }
